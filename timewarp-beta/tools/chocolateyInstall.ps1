eb4e6187628f228cdd7865c2b6c73613b9a21fa4
nxstynate <nathanael.paul.lewis@gmail.com>
2023-11-11 01:06:05 -0600

    commiting the latest files

--- timewarp-beta/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = 'timewarp'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installDir = Join-Path $toolsDir 'timewarp'
-
-Install-ChocolateyZipPackage -PackageName $packageName -Url 'URL_TO_YOUR_PACKAGED_BINARY' -UnzipLocation $installDir
-
--- timewarp-beta/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-$packageName = 'timewarp'
-$installDir = Get-ToolsLocation $packageName
-
-Remove-Item $installDir -Force -Recurse
-
